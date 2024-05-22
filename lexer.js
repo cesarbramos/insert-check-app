f2240f16ff7f80c680c901447cb5ff1286987cb2
cesarbramos <bolanoramoscesaralberto@gmail.com>
2024-05-22 00:12:35 -0500

    identificador empieza por numeros

--- lexer.js
+++ lexer.js
@@ -5,11 +5,12 @@ export class Lexer {
         this.line = 1;
         this.column = 1;
 
-        this.regex = /(^(start\s+transact|begin\s+work)\s*;\s*(insert\s+into\s+((?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*(?<metxhod>(\s*\(\s*(?:(?:[a-zA-Z_$])[0-9a-zA-Z$_]*|(?:[`]\d+[`])|["]\d+["]|(?:["][a-zA-Z_$])[0-9a-zA-Z$_]*["]|[`][a-zA-Z_$][0-9a-zA-Z$_]*[`])(?:\s*,\s*(?:[a-zA-Z_$])[0-9a-zA-Z$_]*|(?:[`]\d+[`])|["]\d+["]|(?:["][a-zA-Z_$])[0-9a-zA-Z$_]*["]|[`][a-zA-Z_$][0-9a-zA-Z$_]*[`])*\s*\)\s*values\s*\(\s*((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)(?:\s*,\s*(?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null))*)\s*\)\s*)|(set\s+(?<mxfields>(?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*=\s*(((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)))(,\s*(?<xmfields>(?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*=\s*(((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)))\s*)*));)(?:\s*(commit|rollback);))|(insert\s+into\s+((?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*(?<xmethod>(\s*\(\s*(?:(?:[a-zA-Z_$])[0-9a-zA-Z$_]*|(?:[`]\d+[`])|["]\d+["]|(?:["][a-zA-Z_$])[0-9a-zA-Z$_]*["]|[`][a-zA-Z_$][0-9a-zA-Z$_]*[`])(?:\s*,\s*(?:[a-zA-Z_$])[0-9a-zA-Z$_]*|(?:[`]\d+[`])|["]\d+["]|(?:["][a-zA-Z_$])[0-9a-zA-Z$_]*["]|[`][a-zA-Z_$][0-9a-zA-Z$_]*[`])*\s*\)\s*values\s*\(\s*((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)(?:\s*,\s*(?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null))*)\s*\)\s*)|(set\s+(?<mfields>(?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*=\s*(((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)))(,\s*(?<xfields>(?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*=\s*(((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)))\s*)*));)/gi;
+        this.regex = /(^(start\s+transact|begin\s+work)\s*;\s*(insert\s+into\s+((?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*(?<metxhod>(\s*\(\s*(?:(?:[a-zA-Z_$])[0-9a-zA-Z$_]*|(?:[`]\d+[`])|["]\d+["]|(?:["][a-zA-Z_$])[0-9a-zA-Z$_]*["]|[`][a-zA-Z_$][0-9a-zA-Z$_]*[`])(?:\s*,\s*(?:[a-zA-Z_$])[0-9a-zA-Z$_]*|(?:[`]\d+[`])|["]\d+["]|(?:["][a-zA-Z_$])[0-9a-zA-Z$_]*["]|[`][a-zA-Z_$][0-9a-zA-Z$_]*[`])*\s*\)\s*values\s*\(\s*((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)(?:\s*,\s*(?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null))*)\s*\)\s*)|(set\s+(?<mxfields>(?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*=\s*(((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)))(,\s*(?<xmfields>(?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*=\s*(((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)))\s*)*));)(?:\s*(commit|rollback);))|(insert\s+into\s+((?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][0-9,a-z,A-Z$_]*[`])\s*(?<xmethod>(\s*\(\s*(?:(?:[a-zA-Z_$])[0-9a-zA-Z$_]*|(?:[`]\d+[`])|["]\d+["]|(?:["][a-zA-Z_$])[0-9a-zA-Z$_]*["]|[`][a-zA-Z_$][0-9a-zA-Z$_]*[`])(?:\s*,\s*(?:[a-zA-Z_$])[0-9a-zA-Z$_]*|(?:[`]\d+[`])|["]\d+["]|(?:["][a-zA-Z_$])[0-9a-zA-Z$_]*["]|[`][a-zA-Z_$][0-9a-zA-Z$_]*[`])*\s*\)\s*values\s*\(\s*((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)(?:\s*,\s*(?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null))*)\s*\)\s*)|(set\s+(?<mfields>(?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*=\s*(((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)))(,\s*(?<xfields>(?:[a-zA-Z_$])[0-9,a-z,A-Z$_]{0,63}|[`][a-zA-Z_$][0-9,a-z,A-Z$_]*[`])\s*=\s*(((?:'[^']*'|[+-]?\d+(?:\.\d+)?|true|false|null)))\s*)*));)/gi;
     }
 
     tokenize(input) {
         this.tokens = [];
+        this.current = 0;
         this.line = 1;
         this.column = 1;
         let match;
@@ -53,8 +54,7 @@ export class Lexer {
     expectToken(expected) {
         const token = this.nextToken();
         const isArr = Array.isArray(expected);
-        if (!token || (!isArr && token.value !== expected) 
-            || isArr && expected.includes(token.value)) {
+        if (!token || (!isArr && token.value !== expected) || (isArr && !expected.includes(token.value))) {
             throw new Error(`Se esperaba el token '${expected}' pero se encontró '${token ? token.value : 'null'}' en la línea ${token ? token.line : this.line}, columna ${token ? token.column : this.column}`);
         }
     }
